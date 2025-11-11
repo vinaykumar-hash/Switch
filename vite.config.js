21f91bdd17fa0fe1c5a5e942258bb22f49fdd9a0
vinaykumar-hash <vinaykumar7525@gmail.com>
2025-11-12 02:36:17 +0530

    signup check

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
